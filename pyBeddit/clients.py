28fcc96077b85c2a21636a5b3c93c344ba6f30b5
Matt <matt.leach25@gmail.com>
2014-11-12 01:41:26 +0000

    adding get_sleep_scores function

--- pyBeddit/clients.py
+++ pyBeddit/clients.py
@@ -21,3 +21,9 @@ class BedditClient(object):
         sleep_data = SleepDataResource(data_dict)
         return sleep_data
     
+    def get_sleep_scores(self):
+        r = self.requestor.get_all_sleeps("2014-01-01", "2015-01-01")
+        
+        for sleep_dict in r.json():
+            sleep_resource = SleepDataResource(sleep_dict)
+            print sleep_resource.date, sleep_resource.properties.total_sleep_score
